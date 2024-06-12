486a43300157c9e50bc01204520eddfe089d3dc5
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-06-11 22:36:28 -0400

    Add files via upload

---
+++ Practica.py
@@ -0,0 +1,15 @@
+#En Python, una lista de listas es simplemente una lista que contiene otras listas como sus elementos. Esta estructura permite crear matrices o tablas bidimensionales, donde cada sublista representa una fila de la matriz.
+#Ejemplo de Lista de Listas
+
+#Supongamos que queremos representar una matriz 3x3. Podemos hacerlo utilizando una lista de listas:
+
+matriz = [[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+
+#En este ejemplo, matriz es una lista que contiene tres sublistas. Cada sublista representa una fila de la matriz.
+#Acceso a Elementos
+
+#Puedes acceder a los elementos de una lista de listas utilizando dos índices: el primero para seleccionar la sublista (fila) y el segundo para seleccionar el elemento dentro de esa sublista (columna).
+
+#Por ejemplo, para acceder al elemento en la segunda fila y tercera columna (valor 6 en nuestro ejemplo), usarías:
